4cb6fc09d948c9566ac002f29d019c1e8763c10f
Gustavo Henrique <gustavohlma8@gmail.com>
2024-08-20 21:06:08 -0300

    allowing vercel domain

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:3000', 'https://seusite.com'];
+const allowedOrigins = ['http://localhost:3000', 'https://strayhome-server.onrender.com/, https://strayhome-client.vercel.app'];
 
 app.use(cors({
   origin: function (origin, callback) {
